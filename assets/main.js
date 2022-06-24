30671c59107020ec11152e821668016cbf4a1620
pujiragil <ragilsg164@gmail.com>
2022-06-24 07:55:49 +0700

    adding smooth scroll

--- assets/main.js
+++ assets/main.js
@@ -5,3 +5,12 @@ btnNav.addEventListener('click', function() {
     btnNav.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+document.querySelectorAll('li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
